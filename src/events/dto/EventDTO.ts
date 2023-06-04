dd6d9ccb134432a37134d9e9a5bf1b2748ba3d81
ANNIHILATOR25 <134809180+ANNIHILATOR25@users.noreply.github.com>
2023-06-04 13:58:33 +0300

    Add files via upload

--- src/events/dto/EventDTO.ts
+++ src/events/dto/EventDTO.ts
@@ -1,9 +1,31 @@
 import { Hall } from "src/halls/hall.entity"
+import { Type } from "class-transformer"
+import { ApiProperty } from "@nestjs/swagger"
+import { IsInt, IsNotEmpty, IsNumber, IsString, Length, Max, Min, ValidateNested} from "class-validator"
 
 export class CreateEventDto {
-  id: number;
-  EventName: string;
-  EventType: string;
-  HallId: number;
+  
+  @ApiProperty({example: 'Boletarian Legends', description: 'Event Name'})
+  @IsString()
+  @Length(2, 50)
+  @IsNotEmpty({ message: 'Event name is required' })
+  EventName: string
+
+  @ApiProperty({example: 'Lection', description: 'Event Type'})
+  @IsString()
+  @Length(2, 50)
+  @IsNotEmpty({ message: 'Event type is required' })
+  EventType: string
+
+  @ApiProperty({ example: {id: 2}, description: 'Hall Id' })
+  @ValidateNested()
+  @Type(() => Hall)
+  @IsNotEmpty({ message: 'Hall id is required' })
+  HallId: number
+
+  @ApiProperty({ example: {id: 2}, description: 'Halls' })
+  @ValidateNested()
+  @Type(() => Hall)
+  @IsNotEmpty({ message: 'Halls are required' })
   halls: Hall[];
 }
