8d454de81a37c7fcd0cd36edd46febc125e1bf03
ANNIHILATOR25 <134809180+ANNIHILATOR25@users.noreply.github.com>
2023-06-04 13:57:56 +0300

    Add files via upload

--- src/staff/dto/StaffDTO.ts
+++ src/staff/dto/StaffDTO.ts
@@ -1,10 +1,39 @@
 import { Hall } from "src/halls/hall.entity"
+import { Type } from "class-transformer"
+import { ApiProperty } from "@nestjs/swagger"
+import { IsInt, IsNotEmpty, IsNumber, IsString, Length, Max, Min, ValidateNested} from "class-validator"
 
 export class CreateStaffDto {
-  id: number;
-  Name: string;
-  position: string;
-  HallId: number;
+
+  @ApiProperty({example: 'Garl Vinland', description: 'Staff Name'})
+  @IsString()
+  @Length(2, 50)
+  @IsNotEmpty({ message: 'Staff name is required' })
+  Name: string
+
+  @ApiProperty({example: 'Security', description: 'Staff Occupation'})
+  @IsString()
+  @Length(2, 50)
+  @IsNotEmpty({ message: 'Staff occupation is required' })
+  position: string
+
+  @ApiProperty({ example: {id: 2}, description: 'Hall Id' })
+  @ValidateNested()
+  @Type(() => Hall)
+  @IsNotEmpty({ message: 'Hall id is required' })
+  HallId: number
+
+  @ApiProperty({example: 1000, description: 'Salary Amount'})
+  @IsNumber()
+  @IsInt()
+  @Min(0)
+  @Max(10000)
+  @IsNotEmpty({ message: 'Salary Amount is required' })
   salary: number;
+  
+  @ApiProperty({ example: {id: 2}, description: 'Halls' })
+  @ValidateNested()
+  @Type(() => Hall)
+  @IsNotEmpty({ message: 'Halls are required' })
   halls: Hall[];
 }
