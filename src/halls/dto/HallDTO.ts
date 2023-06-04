1f0d22cd6950d2f23de3bd2970842b2a03e13d8b
ANNIHILATOR25 <134809180+ANNIHILATOR25@users.noreply.github.com>
2023-06-04 14:00:01 +0300

    Add files via upload

--- src/halls/dto/HallDTO.ts
+++ src/halls/dto/HallDTO.ts
@@ -1,11 +1,33 @@
 import { Expo } from "src/expos/expo.entity"
 import { Staff } from "src/staff/staff.entity"
-import { Event } from 'src/events/event.entity';
+import { Event } from "src/events/event.entity"
+import { Type } from "class-transformer"
+import { ApiProperty } from "@nestjs/swagger"
+import { IsInt, IsNotEmpty, IsNumber, IsString, Length, Max, Min, ValidateNested} from "class-validator"
 
 export class CreateHallDto {
-  id: number;
-  HallName: string;
-  expos: Expo[];
-  staff: Staff[];
+
+  @ApiProperty({ example: 'Lathria', description: 'Hall Name' })
+  @IsString()
+  @Length(2, 50)
+  @IsNotEmpty({ message: 'Name is required' })
+  HallName: string
+
+  @ApiProperty({ example: {id: 2}, description: 'Expo Id' })
+  @ValidateNested()
+  @Type(() => Expo)
+  @IsNotEmpty({ message: 'Expo id is required' })
+  expos: Expo[]
+
+  @ApiProperty({ example: {id: 1}, description: 'Staff Id' })
+  @ValidateNested()
+  @Type(() => Staff)
+  @IsNotEmpty({ message: 'Staff id is required' })
+  staff: Staff[]
+
+  @ApiProperty({ example: {id: 3}, description: 'Event Id' })
+  @ValidateNested()
+  @Type(() => Event)
+  @IsNotEmpty({ message: 'Event id is required' })
   events: Event[];
 }
